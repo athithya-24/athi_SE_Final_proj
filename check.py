6966ea6fdec35dcf1decea8cbe1133f176da394a
athithya-24 <athithyaprakash24@gmail.com>
2025-06-03 11:16:29 +0530

    Remove all files except athithya.py

--- check.py
+++
@@ -1,5 +0,0 @@
-from dpp2 import Product, db, app  # Import app to set up the application context
-
-with app.app_context():  # Set up Flask application context
-    all_products = Product.query.all()
-    print(f"Total products in DB: {len(all_products)}")
